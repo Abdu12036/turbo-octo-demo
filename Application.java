8130ae322c9c9afc38f4676af13167fc91d734e3
Abdunur <amangeldievabdunur@gmail.com>
2025-05-30 20:44:04 +0600

    third commit

--- Application.java
+++ Application.java
@@ -1,9 +1,13 @@
 public class Application{
     public static void main(String[] args){
-        sayHello();
+        System.out.println(get());
     }
 
     public static void sayHello(){
         System.out.println("Hello!");
+    }
+
+    public static int get(){
+        return 5;
     }
 }
